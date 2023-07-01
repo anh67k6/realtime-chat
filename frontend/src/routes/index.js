331e19359967c4cafb11d0a68f0ca17f546b23b9
anhbi <maihoanganhyb2002@gmail.com>
2023-07-02 00:58:56 +0700

    add LoginPage

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -7,6 +7,7 @@ import DashboardLayout from "../layouts/dashboard";
 // config
 import { DEFAULT_PATH } from "../config";
 import LoadingScreen from "../components/LoadingScreen";
+import MainLayout from "../layouts/main";
 
 const Loadable = (Component) => (props) => {
   return (
@@ -18,6 +19,15 @@ const Loadable = (Component) => (props) => {
 
 export default function Router() {
   return useRoutes([
+    {
+      path: "/auth",
+      element: <MainLayout />,
+      children: [
+        { path: "login", element: <LoginPage /> },
+      ],
+
+    },
+
     {
       path: "/",
       element: <DashboardLayout />,
@@ -32,6 +42,8 @@ export default function Router() {
     { path: "*", element: <Navigate to="/404" replace /> },
   ]);
 }
+
+const LoginPage = Loadable(lazy(() => import("../pages/auth/Login")));
 
 const GeneralApp = Loadable(
   lazy(() => import("../pages/dashboard/GeneralApp")),
